28a9bd20132a408811672af6f6a01c12929761ab
pfunzo <pfunzo@synthesis.co.za>
2025-02-15 17:24:54 +0200

    chore : updated keymap for keychron k8( symbol layer and navigation layer)

--- keyboards/keychron/k8/keymaps/homerow/keymap.c
+++ keyboards/keychron/k8/keymaps/homerow/keymap.c
@@ -2,7 +2,7 @@
  * Copyright 2020 Dimitris Mantzouranis <d3xter93@gmail.com>
  * Copyright 2022 Harrison Chan (Xelus)
  * Copyright 2022 Thomas Bowman Mørch (tbowmo)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -17,85 +17,146 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
+ #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ // Each layer gets a name for readability, which is then used in the keymap matrix below.
+ // The underscores don't mean anything - you can have a layer called STUFF or any other name.
+ // Layer names don't all need to be of the same length, obviously, and you can also skip them
+ // entirely and just use numbers.
+ enum layer_names {
+     WIN_BASE    = 0,
+     WIN_FN      = 1,
+     MAC_BASE    = 2,
+     MAC_FN      = 3,
+     WIN_SYM     = 4,
+ };
+ 
+ #define KC_TASK LGUI(KC_TAB)        // Task viewer
+ #define KC_FLXP LGUI(KC_E)          // Windows file explorer
+ #define KC_CRTN LGUI(KC_C)          // Cortana | Microsoft Teams
+ 
+ #define KC_MSSN LGUI(KC_F3)         // Mission Control
+ #define KC_FIND LALT(LGUI(KC_SPC))  // Finder
+ #define KC_SIRI LGUI(KC_SPC)        // Siri
+ #define KC_MSNP LSFT(LGUI(KC_4))    // Mac snip tool
 
 
-/*				+--------------------------------------------------------------------------+----------------+
-        | ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|????|PAUS|
-        +--------------------------------------------------------------------------+------|----|----|
-         |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |SCRL|PGUP|
-         +--------------------------------------------------------------------------+------|----|----|
-         |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
-         +--------------------------------------------------------------------------+------|----|----|
-         | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
-         +--------------------------------------------------------------------------+      |----|    |
-         | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
-         +--------------------------------------------------------------------------+------|----|----|
-         |LCTRL| LGUI| LALT |            SPACE            | RALT| RGUI | FN | RCTRL | |LFT |DWN |RGT |
-         +--------------------------------------------------------------------------+----------------+
-*/
-    [0] = LAYOUT_tkl_ansi(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,      KC_PSCR, KC_SLCK, KC_PAUS,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                       KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(1),   KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-    [1] = LAYOUT_tkl_ansi(
-        RESET,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     RGB_SPI, RGB_SAI, RGB_HUI,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     RGB_SPD, RGB_SAD, RGB_HUD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                       RGB_VAI,
-        _______, _______, _______,                   _______,                                     _______, _______, _______, _______,     RGB_RMOD,RGB_VAD, RGB_MOD
-    ),
-    [2] = LAYOUT_tkl_ansi(
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                       _______,
-        _______, _______, _______,                   _______,                                     _______, _______, _______, _______,     _______, _______, KC_TRNS
-    ),
-    [3] = LAYOUT_tkl_ansi(
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                       _______,
-        _______, _______, _______,                   _______,                                     _______, _______, _______, _______,     _______, _______, KC_TRNS
+ // Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+ 
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*	Windows layout
+     +--------------------------------------------------------------------------+----------------+
+     | ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|????|PAUS|
+     +--------------------------------------------------------------------------+------|----|----|
+     |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |SCRL|PGUP|
+     +--------------------------------------------------------------------------+------|----|----|
+     |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
+     +--------------------------------------------------------------------------+------|----|----|
+     | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
+     +--------------------------------------------------------------------------+      |----|    |
+     | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
+     +--------------------------------------------------------------------------+------|----|----|
+     |LCTRL| LGUI| LALT |            SPACE            | RALT| RGUI | FN | RCTRL | |LFT |DWN |RGT |
+     +--------------------------------------------------------------------------+----------------+
+ */
+ 
+     [WIN_BASE] = LAYOUT_tkl_ansi(
+         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,      KC_PSCR,  KC_CRTN,  RGB_MOD,
+         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,     KC_INS,   KC_HOME,  KC_PGUP,
+         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_DEL,   KC_END,   KC_PGDN,
+         MT(WIN_FN, KC_ESC) HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,  KC_QUOT,            KC_ENT,
+         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                          KC_UP,
+         KC_LCTL, KC_LGUI, KC_LALT,                   LT(WIN_SYMBOLS, KC_SPC),                                      KC_RALT, KC_RGUI, MO(WIN_FN),KC_RCTL,     KC_LEFT,  KC_DOWN,  KC_NO
+     ),
+ 
+     [WIN_FN] = LAYOUT_tkl_ansi(
+         QK_BOOT,          KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,   KC_VOLU,     _______,  _______,  RGB_TOG,
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,     _______,  _______,  _______,
+         _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME,  KC_UP,  KC_END, _______, _______, _______,   _______,     _______,  _______,  _______,
+         _______, HOME_A,  HOME_S,  HOME_D,  HOME_F, _______, KC_PGDN, KC_LEFT, KC_DOWN,  KC_RGHT _______, _______,            _______,
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                          _______,
+         _______, _______, _______,                   _______,                                     _______, _______, _______,   _______,     RGB_SAD,  RGB_SAI,  KC_NO
+     ),
+     [WIN_SYM] = LAYOUT_tkl_ansi(
+      KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP,
+      KC_TAB,   KC_LT,   KC_GT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN,
+      KC_CAPS,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_PIPE, KC_NO,   KC_NO,   KC_DQUO,          KC_ENT,
+      KC_LSFT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                      KC_UP,
+      KC_LCTL,  KC_NO,   KC_NO,                               KC_SPC,                                       KC_RALT, KC_RGUI, KC_NO, KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+ 
+     /*  Mac layout
+     +-------------------------------------------------------------------------------------------+
+     | ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|SIRI|RGB |
+     +--------------------------------------------------------------------------+ +--------------+
+     |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |HOME|PGUP|
+     +--------------------------------------------------------------------------+ +--------------+
+     |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
+     +--------------------------------------------------------------------------+ +--------------+
+     | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
+     +--------------------------------------------------------------------------+      +----+    |
+     | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
+     +--------------------------------------------------------------------------+ +--------------+
+     |LCTRL| LALT| LGUI |            SPACE            | RGUI| RALT | FN | RCTRL | |LFT |DWN |RGT |
+     +-------------------------------------------------------------------------------------------+
+     */
+ 
+     [MAC_BASE] = LAYOUT_tkl_ansi( // Mac base layout
+      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,        KC_MSNP, KC_SIRI, RGB_MOD,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_INS,  KC_HOME, KC_PGUP,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                           KC_UP,
+      KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RGUI, KC_RALT, MO(MAC_FN),KC_RCTL,       KC_LEFT, KC_DOWN, KC_NO
+  ),
+
+  [MAC_FN] = LAYOUT_tkl_ansi( // Mac Fn overlay
+      QK_BOOT,          KC_BRID, KC_BRIU, KC_MSSN, KC_FIND, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,         _______, _______, RGB_TOG,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                           _______,
+      _______, _______, _______,                   _______,                                     _______, _______, _______, _______,         RGB_SAD, _______, KC_NO
     )
-};
-
-bool dip_switch_update_user(uint8_t index, bool active){
-  switch(index){
-    case 0:
-      if(active){ // Win/Android mode
-        // do stuff
-      }
-      else{ // Mac/iOS mode
-        // do stuff
-      }
-      break;
-    case 1:
-      if(active){ //Cable mode
-        // do stuff
-      }
-      else{ //BT mode
-        // do stuff
-      }
-      break; 
-  }
-  return true;
-}
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
+ };
+ 
+ bool dip_switch_update_user(uint8_t index, bool active){
+   switch(index){
+     case 0:
+       if(active) { // Mac mode
+           layer_move(MAC_BASE);
+       } else { // Windows mode
+           layer_move(WIN_BASE);
+       }
+       return false;
+     case 1:
+       if(active){ //Cable mode
+         // do stuff
+       }
+       else{ //BT mode
+         // do stuff
+       }
+       break;
+   }
+ 
+   return true;
+ }
+ 
+ void keyboard_post_init_user(void) {
+   // Customise these values to desired behaviour
+   //debug_enable=true;
+   //debug_matrix=true;
+   //debug_keyboard=true;
+   //debug_mouse=true;
+ }
+ 
